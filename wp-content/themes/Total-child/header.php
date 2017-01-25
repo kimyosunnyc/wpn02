0ffa58e223239d3fd75af5267dde16c49f2e3aef
Ubuntu <ubuntu@ip-172-31-8-35.ap-northeast-2.compute.internal>
2017-01-25 02:56:33 +0000

    homepage content update, edit

--- wp-content/themes/Total-child/header.php
+++ wp-content/themes/Total-child/header.php
@@ -13,6 +13,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
